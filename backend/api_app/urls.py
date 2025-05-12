07e5484fa19853a22ae2b5cadcbd342842feaaee
Yes Plz <119716110+deKoder-ai@users.noreply.github.com>
2025-05-12 22:49:12 +0700

    Fix 404 issue

--- backend/api_app/urls.py
+++ backend/api_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from api_app.views import PatientViewSet
 
-router = routers.DefaultRouter()
+router = DefaultRouter()
 router.register(r'patient', PatientViewSet)
 
 urlpatterns = [
