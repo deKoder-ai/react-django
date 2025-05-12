9122b6b6ffb7654399d55876f07c25d398075cdb
Yes Plz <119716110+deKoder-ai@users.noreply.github.com>
2025-05-12 23:36:59 +0700

    Update

--- src/components/PatientList.js
+++ src/components/PatientList.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { getpatient } from "../services/ApiService";
+import AddPatient from "./AddPatient";
 
 const PatientList = () => {
   const [patients, setPatients] = useState([]);
+  const [showAddPatient, setShowAddPatient] = useState(false);
 
   useEffect(() => {
     let mount = true;
@@ -13,7 +15,56 @@ const PatientList = () => {
     });
   }, []);
 
-  return <div>PatientList</div>;
+  const handleCancelBtn = () => {
+    setShowAddPatient(false);
+    getpatient().then((res) => {
+      console.log("REsponse from api ", res);
+      setPatients(res);
+    });
+  };
+
+  return (
+    <div className="container">
+      <h3>Patient List</h3>
+      <table className="table table-striped table-hover table-bordered">
+        <thead className="table-dark">
+          <tr>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Blood Group</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {patients.map((patient) => (
+            <tr key={patient.patient_id}>
+              <td>{patient.first_name}</td>
+              <td>{patient.last_name}</td>
+              <td>{patient.blood}</td>
+              <td>
+                <button className="btn btn-primary m-2" onClick={() => {}}>
+                  Edit
+                </button>
+                <button className="btn btn-danger" onClick={() => {}}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <br />
+        <button
+          className="btn btn-success"
+          onClick={() => setShowAddPatient(true)}
+        >
+          Add New Patient
+        </button>
+        <br />
+        <br />
+        {showAddPatient && <AddPatient handleCancelBtn={handleCancelBtn} />}
+      </table>
+    </div>
+  );
 };
 
 export default PatientList;
--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export function getpatient() {
-  return axios.get("http:127.0.0.1:8000/patient/").then((res) => res.data);
+  return axios.get("http://127.0.0.1:8000/patient/").then((res) => res.data);
 }
